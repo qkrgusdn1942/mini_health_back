7fecd9c578cea9b1d984fc6bf64db64559109be0
qkrgusdn1942 <89735145+qkrgusdn1942@users.noreply.github.com>
2023-01-15 15:56:35 +0900

    스프링부트 커밋

---
+++ src/main/java/com/mini/health/test/TestController.java
@@ -0,0 +1,15 @@
+package com.mini.health.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+		
+	@GetMapping("/api/test")
+	public String Hellow () {
+		
+		return "HELLOW";
+	}
+	
+}
